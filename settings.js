a2876004db561546df782c6ac8b8d263bbe682e2
caul1007 <98708338+caul1007@users.noreply.github.com>
2023-12-02 09:34:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Loli-Mdོ"
-global.ownername= "ᴹᴿ᭄ Kyouka ×፝֟͜×"
+global.botname = "Rukia-Mdོ"
+global.ownername= "ᴹᴿ᭄ Ren ×፝֟͜×"
 global.myweb ="https://api-alphabot.herokuapp.com/"
-global.youtube = "https://instagram.com/ruka.nime7"
+global.youtube = "https://instagram.com/arsaandii"
 global.github = "https://Neko-Yuri.github.io/"
-global.ownernomer = "6282112080081"
+global.ownernomer = "6283841403395
 global.ownernomerr = "+6282112080081"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6282112080081","6282112080081","6282112080081"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Loli-Mdོ' //sticker wm
-global.author = 'ᴹᴿ᭄ Kyouka ×፝֟͜×' //sticker wm
+global.owner = ["6283841403395","6282112080081","6282112080081"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Rukia-Mdོ' //sticker wm
+global.author = 'ᴹᴿ᭄ Ren ×፝֟͜×' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
